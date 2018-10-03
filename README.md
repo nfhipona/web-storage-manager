78c5deb91d7d0b9096affef457d1651bdc92ca1e
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2018-10-03 19:13:08 +0800

    add web-storage-manager controller

---
+++ README.md
@@ -0,0 +1,144 @@
+# Web Storage Manager
+
+[![npm version]][npm_url]
+
+[npm_url]: https://www.npmjs.com/package/web-storage-manager
+
+## Demo
+
+[Demo Page](https://www.npmjs.com/package/web-storage-manager-example)
+
+## Installation
+
+```bash
+npm install web-storage-manager --save
+```
+
+## Usage
+
+```js
+
+import Storage from 'web-storage-manager';
+
+const keyPaths = [ 'targetKeyOnParent', 'collection', 'targetObject', 'changethis']
+const keyPaths2 = [ 'targetKeyOnParent', 'collection', 'targetObject', 'changethis2']
+
+Storage.updateItemInItem('test-sample-parent-key', keyPaths, valueInObj, 'id')
+Storage.updateItemInItem('test-sample-parent-key', keyPaths2, valueInObj)
+
+```
+
+### Examples
+
+```js
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Storage from 'web-storage-manager';
+
+class App extends Component {
+
+  componentWillMount() {
+
+    // updateItemInItem example
+    const testItems = [
+      {
+        id: 1,
+        value: '777',
+        description: 'test item 1'
+      }, {
+        id: 2,
+        value: '888',
+        description: 'test item 2'
+      }, {
+        id: 3,
+        value: '999',
+        description: 'test item 3'
+      }
+    ]
+
+    const testOjb = {
+      name: 'Object',
+      value: 'target of change',
+      description: 'test item for object type'
+    }
+
+    const tObj = {
+      'changethis': testItems, // key of this
+      'changethis2': testOjb // key of this
+    }
+
+    let collection = {
+      name: 'The data where our target object was saved',
+      'targetObject': tObj // key of this
+    }
+
+    let collectionInfo = {
+      description: 'just another layer for testing',
+      'collection': collection // key of this
+    }
+
+    let parentItem = {
+      name: 'parent item',
+      description: 'test object',
+      'targetKeyOnParent': collectionInfo // key of this
+    }
+
+    Storage.setItem('test-sample', parentItem);
+    Storage.setItem('test-sample-for-compare', parentItem);
+
+    const valueInObj = {
+      id: 2,
+      value: '010',
+      description: 'test item 101'
+    }
+
+    const keyPaths = [ 'targetKeyOnParent', 'collection', 'targetObject', 'changethis']
+    const keyPaths2 = [ 'targetKeyOnParent', 'collection', 'targetObject', 'changethis2']
+    Storage.updateItemInItem('test-sample', keyPaths, valueInObj, 'id')
+    Storage.updateItemInItem('test-sample', keyPaths2, valueInObj)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+```
+
+## Available Functions
+
+```js
+
+storage() // returns window.localStorage
+setItem(key, value)
+setMultiple(items)
+appendItem(key, value)
+updateItemInItem(parentKey, childKeys, value, attrCompare)
+getItem(key)
+getMultiple(keys)
+removeItem(key)
+removeMultiple(keys) [ 'key1', 'key2' ]
+purge() // remove all saved data under active domain
+
+```
---
+++ src/web-storage-manager.js
@@ -0,0 +1,269 @@
+'use strict';
+
+const storage = window.localStorage
+
+const Storage = {
+
+    storage() {
+        return storage
+    },
+
+    /**
+     *
+     * @param {string} key - data key
+     * @param {string} value - data value
+     *
+     */
+    setItem(key, value) {
+
+        try {
+            storage.setItem(key, JSON.stringify(value))
+            return true
+        } catch (error) {
+            return false
+        }
+    },
+
+    /**
+     *
+     * @param {Object[]} items - collection
+     * @param {Object} items[].item - item object
+     * @param {string} items[].item.key - data key
+     * @param {string} items[].item.value - data value
+     *
+     */
+    setMultiple(items) {
+
+        try {
+            for (const i of items) {
+                storage.setItem(i.key, JSON.stringify(i.value))
+            }
+
+            return true
+        } catch (error) {
+            return false
+        }
+    },
+
+    /**
+     *
+     * @param {string} key - data key
+     * @param {string} value - data value
+     *
+     */
+    appendItem(key, value) {
+
+        try {
+            const oldData = this.getItem(key)
+            const newData = {
+                ...oldData,
+                ...value
+            }
+
+            this.setItem(key, newData)
+
+            return true
+        } catch (error) {
+            return false
+        }
+    },
+
+    /**
+     *
+     * @param {string} parentKey - parent data key
+     * @param {string[]} childKeys - data keys - key path
+     * @param {string} value - data value
+     * @param {string} value - data attrib compare
+     *
+     */
+    updateItemInItem(parentKey, childKeys, value, attrCompare) {
+
+        try {
+            let collection = this.getItem(parentKey)
+            if (!collection) return false // terminate process
+
+            let tmpCollection = {}
+
+            // iterate through with child keys
+            for (const [idx, key] of childKeys.entries()) {
+                collection = mapData(key, collection)
+
+                if (idx === childKeys.length - 1) {
+
+                    if (!Array.isArray(collection)) { // check if type object
+                        collection = value // replace with new value
+                    }else{
+                        // collection
+                        const idx = indexOfObject(collection, value, attrCompare)
+
+                        // append or replace object at index
+                        if (idx >= 0) {
+                            collection[idx] = value
+                        }else{
+                            collection.push(value)
+                        }
+                    }
+
+                    // add to temp collection
+                    tmpCollection[key] = collection
+                    mapDataUpdate(this, tmpCollection)
+                }else{
+                    // add to temp collection
+                    tmpCollection[key] = collection
+                }
+            }
+
+            // map data get value from key path
+            function mapData(key, collection) {
+
+                return collection[key]
+            }
+
+            // map data and update collection
+            function mapDataUpdate(self, tmpCollection) {
+
+                const objSelf = Object(self) // set self
+                const oldCollection = objSelf.getItem(parentKey) // get old collection
+                let newCollection = null
+
+                for (const [idx, key] of childKeys.reverse().entries()) { // iterate from last key path first
+
+                    let data = tmpCollection[key]
+
+                    if (!newCollection) {
+                        newCollection = { [key]: data } // set initial value
+                    }else{
+
+                        // update with old data + new data
+                        newCollection = {
+                            [key]: {
+                                ...data,
+                                ...newCollection
+                            }
+                        }
+                    }
+
+                    if (idx === childKeys.length - 1) {
+                        // add modified data to the parent collection
+                        newCollection = {
+                            ...oldCollection,
+                            ...newCollection,
+                        }
+
+                        // save and update local
+                        objSelf.setItem(parentKey, newCollection)
+
+                        return true
+                    }
+                }
+            }
+
+            // Collection ~ Ojbect ~ attribute
+            function indexOfObject(collection, object, attr) {
+
+                for (let i = 0; i < collection.length; i++) {
+                    if (collection[i][attr] === object[attr]) {
+                        return i
+                    }
+                }
+
+                return -1
+            }
+
+        } catch (error) {
+            return false
+        }
+
+    },
+
+    /**
+     *
+     * @param {string} key - key name of your saved data
+     *
+     */
+    getItem(key) {
+
+        try {
+            const data = storage.getItem(key)
+            return JSON.parse(data)
+
+        } catch (error) {
+            return null
+        }
+    },
+
+    /**
+     *
+     * @param {string[]} keys - key names of your saved data
+     *
+     */
+    getMultiple(keys) {
+
+        try {
+            const items = []
+
+            for (const key of keys) {
+                const data = storage.getItem(key)
+                const item = JSON.parse(data)
+
+                if (item) {
+                    items.push(item)
+                }
+            }
+
+            return items
+        } catch (error) {
+            return null
+        }
+    },
+
+    /**
+     *
+     * @param {string} key - key name of your saved data
+     *
+     */
+    removeItem(key) {
+
+        try {
+            storage.removeItem(key)
+            return true
+        } catch (error) {
+            return false
+        }
+    },
+
+    /**
+     *
+     * @param {string[]} keys - key names of your saved data
+     *
+     */
+    removeMultiple(keys) {
+
+        try {
+            for (const key of keys) {
+                storage.removeItem(key)
+            }
+
+            return true
+        } catch (error) {
+            return false
+        }
+    },
+
+    /**
+     *
+     * will purge all saved data under this domain
+     *
+     */
+    purge() {
+
+        try {
+            storage.clear()
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+}
+
+module.exports = Storage;
