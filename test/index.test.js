8288389be32d33fa64239e1a0a5fffdb37aa34c1
nferocious76 <nferocious76@gmail.com>
2023-05-19 14:19:42 +0800

    Update test samples - add deeper level test

--- test/index.test.js
+++ test/index.test.js
@@ -31,6 +31,46 @@ test('Test local storage', function () {
   // we're able to retrieve using window's storage
   const test1a = window.localStorage.getItem('test1');
   expect(test1a).toBe('"test-value-1"');
+
+  // test deeper level updates
+  const parentItem = {
+    name: 'parent item',
+    description: 'test object',
+    'targetKeyOnParent': { id: 1 } // key of this
+  }
+
+  LocalStorage.setItem('test-sample', parentItem);
+  const sourceData = window.localStorage.getItem('test-sample');
+  const parsedSourceData = JSON.parse(sourceData);
+  console.log('parsedSourceData', parsedSourceData);
+  expect(parsedSourceData).toMatchObject(parentItem);
+
+  // update item on key path of previously saved data
+  const keyPath = 'test-sample.targetKeyOnParent'
+  LocalStorage.updateItemInItem(keyPath, { id: 6 }, 'id');
+
+  const test2 = window.localStorage.getItem('test-sample');
+  const parsedTest2 = JSON.parse(test2);
+  console.log('parsedTest2', parsedTest2);
+  expect(parsedTest2).toMatchObject({
+    name: 'parent item',
+    description: 'test object',
+    targetKeyOnParent: { id: 6 }
+  });
+
+  // append item
+  LocalStorage.appendItem('test-sample', { new_item: { desc: 'new test item' } });
+
+  const test3 = window.localStorage.getItem('test-sample');
+  const parsedTest3 = JSON.parse(test3);
+  console.log('parsedTest3', parsedTest3);
+
+  expect(parsedTest3).toMatchObject({
+    name: 'parent item',
+    description: 'test object',
+    targetKeyOnParent: { id: 6 },
+    new_item: { desc: 'new test item' }
+  });
 });
 
 test('Test session storage', function () {
@@ -52,4 +92,44 @@ test('Test session storage', function () {
   // we're able to retrieve using window's storage
   const test1a = window.sessionStorage.getItem('test1');
   expect(test1a).toBe('"test-value-1"');
+
+  // test deeper level updates
+  const parentItem = {
+    name: 'parent item',
+    description: 'test object',
+    'targetKeyOnParent': { id: 7 } // key of this
+  }
+
+  SessionStorage.setItem('test-sample', parentItem);
+  const sourceData = window.sessionStorage.getItem('test-sample');
+  const parsedSourceData = JSON.parse(sourceData);
+  console.log('parsedSourceData', parsedSourceData);
+  expect(parsedSourceData).toMatchObject(parentItem);
+
+  // update item on key path of previously saved data
+  const keyPath = 'test-sample.targetKeyOnParent'
+  SessionStorage.updateItemInItem(keyPath, { id: 8 }, 'id');
+
+  const test2 = window.sessionStorage.getItem('test-sample');
+  const parsedTest2 = JSON.parse(test2);
+  console.log('parsedTest2', parsedTest2);
+  expect(parsedTest2).toMatchObject({
+    name: 'parent item',
+    description: 'test object',
+    targetKeyOnParent: { id: 8 }
+  });
+
+  // append item
+  SessionStorage.appendItem('test-sample', { new_item: { desc: 'new test item' } });
+
+  const test3 = window.sessionStorage.getItem('test-sample');
+  const parsedTest3 = JSON.parse(test3);
+  console.log('parsedTest3', parsedTest3);
+
+  expect(parsedTest3).toMatchObject({
+    name: 'parent item',
+    description: 'test object',
+    targetKeyOnParent: { id: 8 },
+    new_item: { desc: 'new test item' }
+  });
 });
