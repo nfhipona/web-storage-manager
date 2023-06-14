9bdb19287213f1e74b7b348da1f574f5804f1a89
nferocious76 <nferocious76@gmail.com>
2023-06-14 11:54:11 +0800

    Add functions to storage typescript interface

--- src/class/interface.ts
+++ src/class/interface.ts
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Storage
 
 export type KeyPath = string;
-export type StorageValue = any | null;
+export type StorageValue = any;
 export type StorageItem = { key: KeyPath, value: StorageValue };
 
 /**
@@ -11,62 +11,91 @@ export type AttributeCompare = { attribute: string, value: string | number };
 
 export interface Storage {
     /**
+     * keypath delimeter. defaults to '.'.
+     */
+    delimiter: string;
+
+    /**
      * Returns an integer representing the number of data items stored in the Storage object.
      */
-    length(): number
+    get length(): number;
 
     /**
      * Returns an integer representing the number of data items stored in the Storage object.
      * @param n When passed a number n, this method will return the name of the nth key in the storage.
      */
-    key(n: number): number
+    key(n: number): number;
 
     /**
      * When passed a key name, will return that key's value.
      * @param {KeyPath} key key name.
      */
-    getItem(key: KeyPath): StorageValue
+    getItem(key: KeyPath): StorageValue;
 
     /**
      * When passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
      * @param {KeyPath} key A string containing the name of the key you want to create/update.
      * @param {StorageValue} value A string containing the value you want to give the key you are creating/updating.
      */
-    setItem(key: KeyPath, value: StorageValue): boolean | Error
+    setItem(key: KeyPath, value: StorageValue): boolean | Error;
 
     /**
      * When passed a key name, will remove that key from the storage.
      * @param {KeyPath} key A string containing the name of the key you want to remove.
      */
-    removeItem(key: KeyPath): void
+    removeItem(key: KeyPath): void;
 
     /**
      * When invoked, will empty all keys out of the storage.
      */
-    clear(): void
+    clear(): void;
 }
 
 export interface WebStorage extends Storage {
     /**
      * Add multiple entries of key value pairs to the storage.
-     * @param {Array.<StorageItem>} items Items to add individually in the storage.
+     * @param {StorageItem[]} items Items to add individually in the storage.
      */
-    setMultipleItems(items: [StorageItem]): boolean | Error
+    setMultipleItems(items: StorageItem[]): boolean | Error;
+
+    /**
+     * Remove multiple entries found in the specified keypaths.
+     * @param {KeyPath[]} keys 
+     */
+    removeMultipleItems(keys: KeyPath[]): void;
+
+    /**
+     * Returns multiple entries found in the specified keypaths.
+     * @param {KeyPath[]} keys 
+     */
+    getMultipleItems(keys: KeyPath[]): StorageValue[];
 
     /**
      * Append item to an existing item on the storage. Works for object and array type data.
-     * @param key key path of the data you want to append to.
-     * @param value data value you want to append to.
+     * @param {KeyPath} key keypath of the data you want to append to.
+     * @param {StorageValue} value data value you want to append to.
      */
-    appendItemInItems(key: KeyPath, value: StorageValue): boolean | Error
+    appendItemInItems(key: KeyPath, value: StorageValue): boolean | Error;
 
     /**
-     * Updates an item in the specified key path.
-     * @param key key path of the data.
-     * @param value value to set.
-     * @param attrCompare data key attribute to be updated.
+     * Updates an item in the specified keypath.
+     * @param {KeyPath} key keypath of the data.
+     * @param {StorageValue} value value to set.
+     * @param {AttributeCompare} attrCompare data key attribute to be updated.
      */
-    updateItemInItem(key: KeyPath, value: StorageValue, attrCompare: AttributeCompare): boolean | Error
+    updateItemInItem(key: KeyPath, value: StorageValue, attrCompare: AttributeCompare): boolean | Error;
 
-    getItemInItem(key: KeyPath): StorageValue
+    /**
+     * Removes an item in the specified keypath.
+     * @param {KeyPath} key keypath of the data.
+     * @param {StorageValue} value value to set.
+     * @param {AttributeCompare} attrCompare data key attribute to be updated.
+     */
+    removeItemInItem(key: KeyPath, value: StorageValue, attrCompare: AttributeCompare): boolean | Error;
+
+    /**
+     * Returns data found in the specified keypath.
+     * @param {KeyPath} key keypath of the data.
+     */
+    getItemInItem(key: KeyPath): StorageValue;
 }
--- src/class/storage.ts
+++ src/class/storage.ts
@@ -1,8 +1,95 @@
-import { Storage } from './interface';
+import { AttributeCompare, KeyPath, Storage, StorageItem, StorageValue, WebStorage } from './interface';
 
-export class WebStorage {
+export class WebStore implements WebStorage {
+    /**
+     * Web store to be used for this session.
+     */
     #storage: Storage;
-    constructor(storage: Storage) {
+    delimiter: string;
+
+    /**
+     * 
+     * @param storage Storage interface to be used and initialized.
+     */
+    constructor(storage: Storage, delimiter: string = '.') {
         this.#storage = storage;
+        this.delimiter = delimiter;
+    }
+
+    get length(): number {
+        return this.#storage.length;
+    }
+
+    key(n: number): number {
+        return this.#storage.key(n);
+    }
+
+    getItem(key: KeyPath): StorageValue {
+        const stringified = this.#storage.getItem(key);
+        const converted = JSON.parse(stringified);
+        return converted;
+    }
+
+    setItem(key: KeyPath, value: StorageValue): boolean | Error {
+        try {
+            const stringified = JSON.stringify(value);
+            this.#storage.setItem(key, stringified);
+            return true;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    removeItem(key: KeyPath): void {
+        this.#storage.removeItem(key);
+    }
+
+    clear(): void {
+        this.#storage.clear();
+    }
+
+    setMultipleItems(items: StorageItem[]): boolean | Error {
+        try {
+            for (const item of items) {
+                const stringified = JSON.stringify(item.value);
+                this.#storage.setItem(item.key, stringified);
+            }
+            return true;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    removeMultipleItems(keys: KeyPath[]): void {
+        for (const key of keys) {
+            this.#storage.removeItem(key);
+        }
+    }
+
+    getMultipleItems(keys: KeyPath[]): StorageValue[] {
+        const items: StorageValue[] = [];
+        for (const key of keys) {
+            const item = this.getItem(key);
+            if (item) {
+                items.push(item);
+            }
+        }
+        return items;
+    }
+
+    appendItemInItems(key: string, value: any): boolean | Error {
+        throw new Error('Method not implemented.');
+    }
+
+    updateItemInItem(key: string, value: any, attrCompare: AttributeCompare): boolean | Error {
+        throw new Error('Method not implemented.');
+    }
+
+    removeItemInItem(key: string, value: any, attrCompare: AttributeCompare): boolean | Error {
+        throw new Error('Method not implemented.');
+    }
+
+    getItemInItem(key: string) {
+        throw new Error('Method not implemented.');
     }
 }
