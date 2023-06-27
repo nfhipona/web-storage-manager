a0a142e55a3f8fb918e44370208d64a17a08f08a
nferocious76 <nferocious76@gmail.com>
2023-06-28 01:52:37 +0800

    Expose EncodedWebStore

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { WebStore } from './class/storage';
+import { EncodedWebStore } from './class/encoded-storage';
 
-function createLocalStorage({ localStorage }: any = window, delimiter: string = '.') {
-    return new WebStore(localStorage, delimiter);
+export interface Options {
+    delimiter?: string,
+    isEncoded: boolean
 }
 
-function createSessionStorage({ sessionStorage }: any = window, delimiter: string = '.') {
-    return new WebStore(sessionStorage, delimiter);
+function createLocalStorage({ localStorage }: any = window, { delimiter = '.', isEncoded = false }: Options) {
+    return isEncoded ? new EncodedWebStore(localStorage, delimiter) : new WebStore(localStorage, delimiter);
+}
+
+function createSessionStorage({ sessionStorage }: any = window, { delimiter = '.', isEncoded = false }: Options) {
+    return isEncoded ? new EncodedWebStore(sessionStorage, delimiter) : new WebStore(sessionStorage, delimiter);
 }
 
 module.exports = {
     createLocalStorage,
     createSessionStorage,
-    LocalStorage: createLocalStorage(window),
-    SessionStorage: createSessionStorage(window),
-    WebStore
+    LocalStorage: createLocalStorage(window, { isEncoded: false }),
+    SessionStorage: createSessionStorage(window, { isEncoded: false }),
+    EncodedLocalStorage: createLocalStorage(window, { isEncoded: true }),
+    EncodedSessionStorage: createSessionStorage(window, { isEncoded: true }),
+    WebStore,
+    EncodedWebStore
 };
