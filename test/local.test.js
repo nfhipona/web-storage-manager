809203923b69ec03ac46a6ff74d3a82173883ba3
nferocious76 <nferocious76@gmail.com>
2023-06-28 18:03:56 +0800

    Update variable name for LocalStorage

--- test/local.test.js
+++ test/local.test.js
@@ -21,24 +21,24 @@ test('Prepare Vitual DOM for `localStorage` testing', function () {
     expect(testVal).toBe('web-storage-manager');
 });
 
-const LocalStorage = new WebStore(window.localStorage);
+const WebStorage = new WebStore(window.localStorage);
 
 test('Test Storage API primitive functions: `.setItem`, `.length`, `.key`, `.removeItem`, `.getItem`', function () {
-    const isSuccess = LocalStorage.setItem('toRemoveKey', 'to-remove-value');
+    const isSuccess = WebStorage.setItem('toRemoveKey', 'to-remove-value');
     expect(isSuccess).toBe(true);
-    expect(LocalStorage.length).toBeGreaterThanOrEqual(1);
-    expect(LocalStorage.key(1)).not.toBeNull();
+    expect(WebStorage.length).toBeGreaterThanOrEqual(1);
+    expect(WebStorage.key(1)).not.toBeNull();
 
-    LocalStorage.removeItem('toRemoveKey');
-    const item = LocalStorage.getItem('toRemoveKey');
+    WebStorage.removeItem('toRemoveKey');
+    const item = WebStorage.getItem('toRemoveKey');
     expect(item).toBeNull();
 });
 
 test('Test `setItem` and `getItem` function', function () {
-    const isSuccess = LocalStorage.setItem('testKey', 'test-value-1');
+    const isSuccess = WebStorage.setItem('testKey', 'test-value-1');
     expect(isSuccess).toBe(true);
 
-    const item = LocalStorage.getItem('testKey');
+    const item = WebStorage.getItem('testKey');
     expect(item).toBe('test-value-1');
 });
 
@@ -53,23 +53,23 @@ test('Test `getItemInItem` function', function () {
         }
     };
 
-    const isSuccess = LocalStorage.setItem('testKey', testObject);
+    const isSuccess = WebStorage.setItem('testKey', testObject);
     expect(isSuccess).toBe(true);
 
-    const item = LocalStorage.getItem('testKey');
+    const item = WebStorage.getItem('testKey');
     expect(item).not.toBeNull();
 
-    const item2 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
+    const item2 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
     expect(item2).toBe('nestedKeyA-target-value');
 
-    const item3 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
+    const item3 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
     expect(item3).toMatchObject({ itemKey: 'itemKey-value', itemKey2: 'itemKey2-target-value' });
 
-    const item4 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'itemKey2' });
+    const item4 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'itemKey2' });
     expect(item4).toBe('itemKey2-target-value');
 
     // this method targets object with property [name] `id` with [value] of `id3`.
-    const item5 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id3' });
+    const item5 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id3' });
     expect(item5).toMatchObject({ id: 'id3' });
 });
 
@@ -81,16 +81,16 @@ test('Test `setMultipleItems` function', function () {
         { key: 'item2Key', value: ['item2AValue', 'item2BValue', 'item2CValue'] },
         { key: 'item3Key', value: { item3AKey: 'item3BValue', item3BKey: 'item3BValue' } }
     ];
-    const isSuccess = LocalStorage.setMultipleItems(items);
+    const isSuccess = WebStorage.setMultipleItems(items);
     expect(isSuccess).toBe(true);
 
-    const item1 = LocalStorage.getItem('item1Key');
+    const item1 = WebStorage.getItem('item1Key');
     expect(item1).toBe('item1Value');
 
-    const item2 = LocalStorage.getItem('item2Key');
+    const item2 = WebStorage.getItem('item2Key');
     expect(item2).toMatchObject(['item2AValue', 'item2BValue', 'item2CValue']);
 
-    const item3 = LocalStorage.getItem('item3Key');
+    const item3 = WebStorage.getItem('item3Key');
     expect(item3).toMatchObject({ item3AKey: 'item3BValue', item3BKey: 'item3BValue' });
 });
 
@@ -99,24 +99,24 @@ test('Test `removeMultipleItems` function', function () {
     // this function utilizes keypath. however, it is not fully supported.
     // it is advised to use `removeItemInItem` with `attrCompare` parameter.
 
-    const item1 = LocalStorage.getItem('item1Key');
+    const item1 = WebStorage.getItem('item1Key');
     expect(item1).not.toBeNull();
 
-    const item2 = LocalStorage.getItem('item2Key');
+    const item2 = WebStorage.getItem('item2Key');
     expect(item2).not.toBeNull();
 
-    const item3 = LocalStorage.getItem('item3Key');
+    const item3 = WebStorage.getItem('item3Key');
     expect(item3).not.toBeNull();
 
-    LocalStorage.removeMultipleItems(['item1Key', 'item2Key', 'item3Key']);
+    WebStorage.removeMultipleItems(['item1Key', 'item2Key', 'item3Key']);
 
-    const item1a = LocalStorage.getItem('item1Key');
+    const item1a = WebStorage.getItem('item1Key');
     expect(item1a).toBeNull();
 
-    const item2a = LocalStorage.getItem('item2Key');
+    const item2a = WebStorage.getItem('item2Key');
     expect(item2a).toBeNull();
 
-    const item3a = LocalStorage.getItem('item3Key');
+    const item3a = WebStorage.getItem('item3Key');
     expect(item3a).toBeNull();
 });
 
@@ -124,7 +124,7 @@ test('Test `getMultipleItems` function', function () {
     // NOTE:
     // this utilizes keypath. however, 
     // it is recommended to use `getItemInItem` for a more granular key search with `attrCompare`.
-    const results = LocalStorage.getMultipleItems([
+    const results = WebStorage.getMultipleItems([
         'testKey.nestedKey.nestedKeyA',
         'testKey.nestedKey.nestedKeyB.nestedKeyC',
         'testKey.nestedKey.nestedKeyB.nestedKeyD'
@@ -149,16 +149,16 @@ test('Test `appendItemInItem` function', function () {
      *      }
      *  };
      */
-    const isSuccess1 = LocalStorage.appendItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { appendItemInItemAKey: 'appendItemInItemA-target-appended-value' });
+    const isSuccess1 = WebStorage.appendItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { appendItemInItemAKey: 'appendItemInItemA-target-appended-value' });
     expect(isSuccess1).toBe(true);
 
-    const isSuccess2 = LocalStorage.appendItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { id: 'id4', value: 'appendItemInItemB-target-appended-value' });
+    const isSuccess2 = WebStorage.appendItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { id: 'id4', value: 'appendItemInItemB-target-appended-value' });
     expect(isSuccess2).toBe(true);
 
-    const result1 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
+    const result1 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
     expect(result1).toMatchObject({ itemKey: 'itemKey-value', itemKey2: 'itemKey2-target-value', appendItemInItemAKey: 'appendItemInItemA-target-appended-value' });
 
-    const result2 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD');
+    const result2 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD');
     expect(result2).toMatchObject([{ id: 'id1' }, { id: 'id2' }, { id: 'id3' }, { id: 'idz' }, { id: 'id4', value: 'appendItemInItemB-target-appended-value' }]);
 });
 
@@ -176,22 +176,22 @@ test('Test `updateItemInItem` function', function () {
      *  };
      */
 
-    const isSuccess1 = LocalStorage.updateItemInItem('testKey.nestedKey.nestedKeyA', null, 'nestedKeyA-target-updated-value');
+    const isSuccess1 = WebStorage.updateItemInItem('testKey.nestedKey.nestedKeyA', null, 'nestedKeyA-target-updated-value');
     expect(isSuccess1).toBe(true);
 
-    const isSuccess2 = LocalStorage.updateItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'appendItemInItemAKey' }, 'appendItemInItemA-target-appended-updated-value');
+    const isSuccess2 = WebStorage.updateItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'appendItemInItemAKey' }, 'appendItemInItemA-target-appended-updated-value');
     expect(isSuccess2).toBe(true);
 
-    const isSuccess3 = LocalStorage.updateItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id4' }, { id: 'id4', value: 'appendItemInItemB-target-appended-updated-value' });
+    const isSuccess3 = WebStorage.updateItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id4' }, { id: 'id4', value: 'appendItemInItemB-target-appended-updated-value' });
     expect(isSuccess3).toBe(true);
 
-    const result1 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
+    const result1 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
     expect(result1).toBe('nestedKeyA-target-updated-value');
 
-    const result2 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'appendItemInItemAKey' });
+    const result2 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'appendItemInItemAKey' });
     expect(result2).toBe('appendItemInItemA-target-appended-updated-value');
 
-    const result3 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id4' });
+    const result3 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id4' });
     expect(result3).toMatchObject({ id: 'id4', value: 'appendItemInItemB-target-appended-updated-value' });
 });
 
@@ -208,26 +208,26 @@ test('Test `removeItemInItem` function', function () {
      *      }
      *  };
      */
-    const isSuccess1 = LocalStorage.removeItemInItem('testKey.nestedKey.nestedKeyA');
+    const isSuccess1 = WebStorage.removeItemInItem('testKey.nestedKey.nestedKeyA');
     expect(isSuccess1).toBe(true);
 
-    const isSuccess2 = LocalStorage.removeItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'itemKey2' });
+    const isSuccess2 = WebStorage.removeItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC', { name: 'itemKey2' });
     expect(isSuccess2).toBe(true);
 
-    const isSuccess3 = LocalStorage.removeItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id3' });
+    const isSuccess3 = WebStorage.removeItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD', { name: 'id', value: 'id3' });
     expect(isSuccess3).toBe(true);
 
-    const result1 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
+    const result1 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyA');
     expect(result1).toBeUndefined();
 
-    const result2 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
+    const result2 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyC');
     expect(result2).toMatchObject({ itemKey: 'itemKey-value', appendItemInItemAKey: 'appendItemInItemA-target-appended-updated-value' });
 
-    const result3 = LocalStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD');
+    const result3 = WebStorage.getItemInItem('testKey.nestedKey.nestedKeyB.nestedKeyD');
     expect(result3).toMatchObject([{ id: 'id1' }, { id: 'id2' }, { id: 'idz' }, { id: 'id4', value: 'appendItemInItemB-target-appended-updated-value' }]);
 });
 
 test('Test `clear` function', function () {
-    LocalStorage.clear();
-    expect(LocalStorage.length).toBe(0);
+    WebStorage.clear();
+    expect(WebStorage.length).toBe(0);
 });
